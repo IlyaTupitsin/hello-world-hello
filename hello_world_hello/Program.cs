6fd0e7a93f9ffee56955b18f3ffc4b7e7d4e7f97
Илья Тупицин <ilhatupitsin@yandex.ru>
2024-05-13 12:24:14 +0300

    Добавьте файлы проекта.

---
+++ hello_world_hello/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку:");
+        string input = Console.ReadLine();
+
+        string pattern = @"\b(\w+)\b(.*?)\b\1\b";
+        Regex rgx = new Regex(pattern);
+
+        string result = rgx.Replace(input, "<strong>$1</strong>$2<strong>$1</strong>");
+        Console.WriteLine(result);
+    }
+}
